37b26fce111f07cc45284ac96c0721fcc74c109d
Luiz Henrique Nascimento dos Anjos <lhenriqueanjos@gmail.com>
2016-06-05 19:18:27 -0300

    tela de pesquisa de salas

--- webapp/sala/pesquisar.php
+++ webapp/sala/pesquisar.php
@@ -2,6 +2,51 @@
 	$current_page = "sala";
 	require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/header.php";
 	require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/menu.php";
+
+	$total = 0;
+	$linha = NULL;
+
+	// verifica se já clicou no pesquisar
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+		// abrir conexão
+		require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/conexao.php";
+
+		// variáveis para insert
+		$nomeSala = $_POST['txtNome'];
+		$numeroSala = $_POST['txtNumero'];
+		$descricaoSala = $_POST['txtDescricao'];
+
+		// evitar sql inject
+		$nomeSala = mysql_escape_string($nomeSala);
+		$numeroSala = mysql_escape_string($numeroSala);
+		$descricaoSala = mysql_escape_string($descricaoSala);
+
+		$whereAnd = "WHERE";
+
+		// montagem da query
+		$query = "SELECT id, numero, nome, descricao "
+			."FROM sala ";
+
+		if (!empty($nomeSala)) {
+			$query = $query.$whereAnd." nome like '".$nomeSala."%' ";
+			$whereAnd = "AND";
+		}
+		if (!empty($numeroSala)) {
+			$query = $query.$whereAnd." numero = ".$numeroSala." ";
+			$whereAnd = "AND";
+		}
+		if (!empty($descricaoSala)) {
+			$query = $query.$whereAnd." descricao like '%".$descricaoSala."%' ";
+		}
+		$query = $query."order by nome;";
+
+		$resultado = mysql_query($query, $link) or die(mysql_error());
+		
+		$linha = mysql_fetch_assoc($resultado);
+
+		$total = mysql_num_rows($resultado);
+	}
 ?>
 		<div class="col-xs-10">
 			<div class="row">
@@ -12,19 +57,19 @@
 				</div>
 			</div>
 		
-			<form>
+			<form action="#" method="POST">
 				<div class="row">
 					<div class="form-group col-xs-5">
 						<label for="txtNome">Nome:</label>
-						<input type="text" class="form-control" id="txtNome">
+						<input type="text" class="form-control" id="txtNome" name="txtNome">
 					</div>
 					<div class="form-group col-xs-2">
 						<label for="txtNumero">Número:</label>
-						<input type="number" step="0" class="form-control" id="txtNumero">
+						<input type="number" step="0" class="form-control" id="txtNumero" name="txtNumero">
 					</div>
 					<div class="form-group col-xs-5">
 						<label for="txtDescricao">Descrição:</label>
-						<input type="text" class="form-control" id="txtDescricao">
+						<input type="text" class="form-control" id="txtDescricao" name="txtDescricao">
 					</div>
 				</div>
 				
@@ -35,42 +80,82 @@
 						</button>
 					</div>
 				</div>
-				
-				<div class="row">
-					<div class="col-xs-12">
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Salas</h3>
+					</div>
+					<div class="panel-body">
+
+						<?php 
+							// se o usuário tiver feito a pesquisa, exibe a tabela
+							if(isset($_POST) and $total > 0) {
+						?>
 						<table class="table table-bordered table-hover">
 							<thead>
-								<th>Local</th>
+								<th>Nome</th>
 								<th>Número</th>
 								<th>Descrição</th>
 							</thead>
 							<tbody>
+							<?php 
+									// para cada linha retornada, criar um tr para exibir os dados
+									do {
+							 ?>
 								<tr>
-									<td>LABINFO 1</td>
-									<td>1</td> 
-									<td>Labortório de Informática 1</td>
+									<td><?=$linha['nome']?></td>
+									<td><?=$linha['numero']?></td> 
+									<td><?=$linha['descricao']?></td>
 								</tr>
-								<tr>
-									<td>LABINFO 2</td>
-									<td>2</td> 
-									<td>Labortório de Informática 2</td>
-								</tr>
+							<?php 
+									} while($linha = mysql_fetch_assoc($resultado));
+									
+									// tira o resultado da busca da memória
+									mysql_free_result($dados);
+							?>
 							</tbody>
 						</table>
+						<?php 
+							} else { 
+							// caso não tenha pesquisado ou a pesquisa não tiver resultados, exibe mensagem amigável
+						?>
+							<span>
+								Nenhum registro para exibir.
+							</span>
+						<?php 
+							} 
+						?>
+
+						<div class="row">
+							<div class="form-group col-xs-12">
+							</div>
+						</div>
 					</div>
 				</div>
 				
 				<div class="row">
 					<div class="form-group col-xs-12">
+
+						<!-- Botões sempre exibidos -->
+
 						<button type="submit" class="btn btn-default" formaction="cadastrar.php">
 							<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Incluir
 						</button>
-						<button type="submit" class="btn btn-default">
-							<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Editar
-						</button>
-						<button type="submit" class="btn btn-default">
-							<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
-						</button>
+
+						<?php 
+							// se o usuário tiver feito a pesquisa, exibe a tabela
+							if(isset($_POST) and $total > 0) {
+						?>
+							<!-- Botões exibidos apenas se tiver registros na tabela -->
+							<button type="submit" class="btn btn-default">
+								<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Editar
+							</button>
+							<button type="submit" class="btn btn-default">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
+							</button>
+						<?php 
+							} 
+						?>
 					</div>
 				</div>
 			</form>
