8db7fc7c74ab57e70200775e2d546418eab6f5c8
Luiz Henrique Nascimento dos Anjos <lhenriqueanjos@gmail.com>
2016-06-05 17:02:47 -0300

    tela de sucesso para inclusão de sala

--- webapp/sala/cadastrar_sala_action.php
+++ webapp/sala/cadastrar_sala_action.php
@@ -21,15 +21,78 @@ $query = "INSERT INTO sala (nome, numero, descricao) VALUES ('".$nomeSala."', ".
 // Executa a query
 $inserir = mysql_query($query);
 
-if ($inserir) {
-	echo "Sala inserida com sucesso!";
-} else {
+if (!$inserir) {
 	// TODO redirecionar para uma sala de erro padronizada
 	echo "Não foi possível inserir a sala, tente novamente.";
 	// Exibe dados sobre o erro:
 	echo "Dados sobre o erro:" . mysql_error();
 }
 
-// redirecionar para a tela de pesquisa
+$current_page = "sala";
+require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/header.php";
+require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/menu.php";
+
+?>
+
+	<div class="col-xs-10">
+		<div class="row">
+			<div class="col-xs-12">
+				<ol class="breadcrumb">
+					<li>
+						<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/sala/pesquisar.php">Salas</a>
+					</li>
+					<li>
+						<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/sala/cadastrar.php">Incluir</a>
+					</li>
+					<li class="active">Sucesso</li>
+				</ol>
+				</div>
+			</div>
+		
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="alert alert-success" role="alert">
+						Sala incluída com sucesso.
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<p>O que deseja fazer?</p>	
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/sala/pesquisar.php">
+						<span class="glyphicon glyphicon-search" aria-hidden="true"></span> Pesquisar salas
+					</a>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/sala/cadastrar.php">
+						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Incluir uma nova sala
+					</a>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-xs-12">
+					<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/">
+						<span class="glyphicon glyphicon-home" aria-hidden="true"></span> Voltar para o início
+					</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+<?php	
+
+require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/footer.php";
 
 ?>
