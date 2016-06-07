d4bf7d73b443b27cb2d689edc2059f45358e0412
Lucas Campos <perfectlukke@gmail.com>
2016-06-07 19:17:00 -0300

    Ajustes pequenos

--- webapp/tag/pesquisar_tag.php
+++ webapp/tag/pesquisar_tag.php
@@ -13,7 +13,22 @@
 	$total = mysqli_num_rows($result);
     
     $row = mysqli_fetch_assoc($result); 
+	
+	$opt = " ";
+	
+	// verifica se já clicou no pesquisar e consulta o banco de acordo com o select do usuário
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		
+		$opt = $_POST['opt'];
+		
+		// $sqlForm = "SELECT id_usuario FROM tag";
+		// $resultForm = mysqli_query($link, $sql);
 
+		// $totalForm = mysqli_num_rows($result);
+		
+		// $rowForm = mysqli_fetch_assoc($result); 
+		
+	}
 ?>
 		<div class="col-xs-10">
 			<div class="row">
@@ -24,11 +39,11 @@
 				</div>
 			</div>
 
-			<form>
+			<form action="#" method="POST">
 				<div class="row">
 					<div class="form-group col-xs-5">
 						<label for="selTAG">Chave:</label>
-						<select class="form-control" id="selTAG">
+						<select class="form-control" id="selTAG" name="opt">
 							<option selected>selecione </option>
 							<?php
 								do {
@@ -51,39 +66,39 @@
 						</button>
 					</div>
 				</div>
-
-				<form>
-					<div class="row">
-						<div class="form-group col-xs-5">
-							<label for="txtNomeAp">Nome:</label>
-							<input type="text" class="form-control" id="txtNomeAp" disabled>
-						</div>
-						<div class="form-group col-xs-3">
-							<label for="txtTipoAp">Tipo:</label>
-							<input type="text" class="form-control" id="txtTipoAp" disabled>
-						</div>
-					</div>
-				</form>
-				
-				<div class="row">
-					<div class="col-xs-10">
-						<table class="table table-bordered table-hover">
-							<thead>
-								<tr>
-									<th colspan=2>Últimos 5 acessos</td>
-								</tr>
-							</thead>
-							<thead>
-								<th>Acesso</th>
-								<th>Local</th>
-							</thead>
-							<tbody>
-							<?php require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/tag/pesquisar_tag_historico.php"; ?>
-							</tbody>
-						</table>
-					</div>
-				</div>				
 			</form>
+			
+			<form>
+				<div class="row">
+					<div class="form-group col-xs-5">
+						<label for="txtNomeAp">Nome:</label>
+						<input type="text" class="form-control" id="txtNomeAp" value="<?=$opt ?>" disabled> 
+					</div>
+					<div class="form-group col-xs-3">
+						<label for="txtTipoAp">Tipo:</label>
+						<input type="text" class="form-control" id="txtTipoAp" value="<?=$opt ?>" disabled>
+					</div>
+				</div>
+			</form>
+			
+			<div class="row">
+				<div class="col-xs-10">
+					<table class="table table-bordered table-hover">
+						<thead>
+							<tr>
+								<th colspan=2>Últimos 5 acessos</td>
+							</tr>
+						</thead>
+						<thead>
+							<th>Acesso</th>
+							<th>Local</th>
+						</thead>
+						<tbody>
+						<?php include $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/tag/pesquisar_tag_historico.php"; ?>
+						</tbody>
+					</table>
+				</div>
+			</div>				
 		</div>
 <?php	
 	require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/footer.php";
--- webapp/tag/pesquisar_tag_historico.php
+++ webapp/tag/pesquisar_tag_historico.php
@@ -2,19 +2,23 @@
 
 require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/conexao.php";
 
-$sql = "SELECT DATE_FORMAT(data_hora, '%d/%m/%Y %H:%i:%s') as data_hora, id_sala, id FROM acesso"; // TODO -> associar ao usuário logado, para puxar o histórico correto
+$sql = "SELECT DATE_FORMAT(data_hora, '%d/%m/%Y %H:%i:%s') as data_hora, id_sala, id FROM acesso ORDER BY data_hora DESC"; // TODO -> associar ao usuário logado, para puxar o histórico correto
 $result = mysqli_query($link, $sql);
+
+$count = 0;
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
+        if ($count < 5){
+		echo "<tr>";
 		$queryCategoria = "SELECT nome FROM sala WHERE id = ".$row['id_sala']." ";
 		$resultado2 = mysqli_query($link, $queryCategoria) or die(mysqli_error($link));
 		$linha = mysqli_fetch_assoc($resultado2);
 		echo "<td>" .$row["data_hora"]. "</td><td>" .$linha["nome"]. "</td>";
 		echo "</tr>";
+		$count++;
+		}
     }
 } 
-
 ?>
