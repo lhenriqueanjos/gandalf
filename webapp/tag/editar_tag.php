3f4f70e9aa0f1d93f0e3254028ae8a222ce9d5bc
Lucas Campos <perfectlukke@gmail.com>
2016-06-08 19:18:20 -0300

    Implementado a pagina de edição de TAG

--- webapp/tag/editar_tag.php
+++ webapp/tag/editar_tag.php
@@ -2,6 +2,30 @@
 	$current_page = "tag";
 	require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/header.php";
 	require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/menu.php";
+	
+	// abre a conexão
+	require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/conexao.php";
+	
+	$auxiliar = $_POST['optChave'];
+	
+	// query para carregar o select do tipo da tag
+	$sqlTipoTag = "SELECT id, tipo FROM categoria_tag";
+	$resultTipoTag = mysqli_query($link, $sqlTipoTag);
+
+	$totalTipoTag = mysqli_num_rows($resultTipoTag);
+    
+    $rowTipoTag = mysqli_fetch_assoc($resultTipoTag); 
+	
+	// query para carregar a chave no input desabilitado
+	$sql = "SELECT codigo FROM tag WHERE id = $auxiliar"; // TODO -> associar ao usuário logado, para puxar o histórico correto
+	$result = mysqli_query($link, $sql);
+
+	$total = mysqli_num_rows($result);
+    
+    $row = mysqli_fetch_assoc($result); 
+	
+	$auxiliar2 = $row['codigo'];
+
 ?>
 		<div class="col-xs-10">
 			<div class="row">
@@ -15,19 +39,26 @@
 				</div>
 			</div>
 		
-			<form>
+			<form action="editar_tag_action.php" method="POST">
 				<div class="row">
 					<div class="form-group col-xs-5">
 						<label for="txtChave">Chave:</label>
-						<input type="number" class="form-control" id="txtChave" disabled>
+						<input type="number" class="form-control" id="txtChave" name="txtChave" value="<?php echo $auxiliar2; ?>" readonly>
 					</div>
 					<div class="form-group col-xs-3">
 						<label for="txtTipo">Tipo:</label>
 						<br>
 						<select name="txtTipo" class="form-control">
-							<option value="administrador">Administrador</option>
-							<option value="administrador">Tag/Cartão</option>
-							<option value="administrador" selected>Cliente</option>
+							<?php
+								do {
+							?>
+							<option value="<?=$rowTipoTag['id']?>" <?php $aux = $rowTipoTag['tipo']; if($aux == "Cliente"){echo "selected";} ?>><?=$rowTipoTag['tipo']?></option>
+							<?php
+								}while($rowTipoTag = mysqli_fetch_assoc($resultTipoTag));
+								
+								// tira o resultado da busca da memória
+								mysqli_free_result($resultTipoTag);
+							?>							
 						</select>
 					</div>
 				</div>
@@ -37,7 +68,7 @@
 						<button type="submit" class="btn btn-default">
 							<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Salvar
 						</button>
-						<button type="submit" class="btn btn-default" formaction="pesquisar.php">
+						<button type="submit" class="btn btn-default"  formaction="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/tag/cadastrar_tag.php">
 							<span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span> Cancelar
 						</button>
 					</div>
---
+++ webapp/tag/editar_tag_action.php
@@ -0,0 +1,90 @@
+<?php
+
+$current_page = "tag";
+require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/header.php";
+require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/menu.php";
+
+// abre a conexão
+require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/conexao.php";
+
+// var_dump($_POST);
+
+// variáveis para insert
+$chave = $_POST['txtChave'];
+$idCategoria = $_POST['txtTipo'];
+
+// evitar sql inject
+$chave = mysqli_real_escape_string($link, $chave);
+$idCategoria = mysqli_real_escape_string($link, $idCategoria);
+
+// montagem da query
+$query = "UPDATE tag 
+		SET id_categoria = $idCategoria 
+		WHERE codigo = $chave";
+
+// Executa a query
+$inserir = mysqli_query($link, $query);
+
+if (!$inserir) {
+	// TODO redirecionar para uma sala de erro padronizada
+	echo "Não foi possível alterar a TAG, tente novamente.";
+	// Exibe dados sobre o erro:
+	echo "Dados sobre o erro:" . mysqli_error($link);
+}
+
+?>
+
+	<div class="col-xs-10">
+		<div class="row">
+			<div class="col-xs-12">
+				<ol class="breadcrumb">
+					<li>
+						<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/tag/cadastrar_tag.php">TAG</a>
+					</li>
+					<li>
+						<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/tag/cadastrar_tag.php">Editar</a>
+					</li>
+					<li class="active">Sucesso</li>
+				</ol>
+				</div>
+			</div>
+		
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="alert alert-success" role="alert">
+						TAG alterada com sucesso.
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<p>O que deseja fazer?</p>	
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/tag/cadastrar_tag.php">
+						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Incluir uma nova TAG
+					</a>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-xs-12">
+					<a href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/">
+						<span class="glyphicon glyphicon-home" aria-hidden="true"></span> Voltar para o início
+					</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+<?php	
+
+require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/footer.php";
+
+?>
