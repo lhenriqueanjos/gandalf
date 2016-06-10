a0d501d1baefe80b5a4ac311149b560fb1f6a5fd
Luiz Henrique Nascimento dos Anjos <lhenriqueanjos@gmail.com>
2016-06-09 22:08:48 -0300

    mudança no tema
    mudei o tema, para utilizar cores mais sóbrias, que combinam mais com um
    sistema cujo foco é a segurança

--- webapp/header.php
+++ webapp/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title>Sistema de Controle de Acesso</title>
-	<link rel="stylesheet" href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/resources/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/gandalf/webapp/resources/bootstrap/css/bootstrap.min.css">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf8">
 </head>
 <body>
