d1762c3fcde5ff5ff5b24b6ef4605603c4948bcc
Lucas Campos <perfectlukke@gmail.com>
2016-06-18 00:29:50 -0300

    Campo "Chave" tornado obrigatorio

--- webapp/tag/cadastrar_tag.php
+++ webapp/tag/cadastrar_tag.php
@@ -36,7 +36,7 @@
 				<div class="row">
 					<div class="form-group col-xs-5">
 						<label for="txtChave">Chave:</label>
-						<input type="number" class="form-control" id="txtChave" name="numTag">
+						<input type="number" class="form-control" id="txtChave" name="numTag" required>
 					</div>
 					<div class="form-group col-xs-3">
 						<label for="txtTipo">Tipo:</label>
