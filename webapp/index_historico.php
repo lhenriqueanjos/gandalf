201ca2bd12b7f5649a87a8c25b313e9d23897c26
Luiz Henrique Nascimento dos Anjos <lhenriqueanjos@gmail.com>
2016-07-01 14:36:44 -0300

    exibir corretamente as últimas entradas na index

--- webapp/index_historico.php
+++ webapp/index_historico.php
@@ -2,28 +2,45 @@
 
 require $_SERVER["DOCUMENT_ROOT"]. "/gandalf/webapp/conexao.php";
 
-$sql = "SELECT DATE_FORMAT(data_hora, '%d/%m/%Y %H:%i:%s') as data_hora, acesso.id_sala, acesso.id
-		FROM rel_usuario_tag
-		JOIN acesso
-		ON acesso.id_tag = rel_usuario_tag.id_tag
-		WHERE rel_usuario_tag.id_tag = $idUsuario
-		ORDER BY data_hora DESC";
+$sql = "SELECT DATE_FORMAT(data_hora, '%d/%m/%Y %H:%i:%s') as data_hora, sala.nome as nomeSala, acesso.id 
+		FROM rel_usuario_tag 
+		JOIN acesso 
+		ON acesso.id_tag = rel_usuario_tag.id_tag 
+		JOIN sala on sala.id = acesso.id_sala ";
+
+if (isset($idTag)) {
+	$sql = $sql . "WHERE rel_usuario_tag.id_tag = $idTag ";
+} else {
+	$sql = $sql . "WHERE rel_usuario_tag.id_usuario = $idUsuario ";
+}
+
+$sql = $sql . "ORDER BY data_hora DESC";
 
 $count = 0;
 $result = mysqli_query($link, $sql);
 
+$row = mysqli_fetch_assoc($result);
+
 if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
+
+	echo "<table class=\"table table-bordered table-hover\">
+			<thead>
+				<th>Acesso</th>
+				<th>Local</th>
+			</thead>
+			<tbody>";
+
+	do {
         if ($count < 5){
-		$queryCategoria = "SELECT nome FROM sala WHERE id = ".$row['id_sala']." ";
-		$resultado2 = mysqli_query($link, $queryCategoria) or die(mysqli_error($link));
-		$linha = mysqli_fetch_assoc($resultado2);
-		echo "<tr>";
-		echo "<td>" .$row["data_hora"]. "</td><td>" .$linha["nome"]. "</td>";
-		echo "</tr>";
-		$count++;
+			echo "<tr>";
+			echo "<td>" .$row["data_hora"]. "</td><td>" .$row["nomeSala"]. "</td>";
+			echo "</tr>";
+			$count++;
 		}
-    }
+	} while($row = mysqli_fetch_assoc($result));
+
+	echo "</tbody>
+		</table>";
 }else{
 	echo "Não há acessos recentes!";
 } 
